b01c8a65438bb356a41d3883177859c41405f85a
vincent1879 <vincet1879@gmail.com>
2013-08-22 18:12:49 +0800

    Update mac config

---
+++ ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XGame
+//
+//  Created by Xu Yu on 13-8-22.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
